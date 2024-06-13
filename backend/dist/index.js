1cd327daacc82d9fe35955066816ad10e7306ca4
sagzone <spkawad21@gmail.com>
2024-06-13 21:57:48 +0530

    express server setted up with typescript

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,38 +1,37 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
-function insertUser(username, email, password) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const user = yield prisma.user.create({
-            data: {
-                username,
-                email,
-                password,
-            },
-        });
-        console.log(user);
-    });
-}
-function insertCheatMeal(date, isCheat, userId) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const response = yield prisma.cheatMeal.create({
-            data: {
-                date: new Date(date),
-                isCheat,
-                userId,
-            },
-        });
-    });
-}
-// insertUser("sag", "spk21@gmail.com", "123456");
-insertCheatMeal("2024-06-13T16:30:00", true, 1);
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.get("/register", (req, res) => {
+    res.json({ msg: "on register" });
+});
+app.listen(3000, () => console.log("server up and running on port 3000"));
+// async function insertUser(username: string, email: string, password: string) {
+//   const user = await prisma.user.create({
+//     data: {
+//       username,
+//       email,
+//       password,
+//     },
+//   });
+//   console.log(user);
+// }
+// async function insertCheatMeal(date: string, isCheat: boolean, userId: number) {
+//   const response = await prisma.cheatMeal.create({
+//     data: {
+//       date: new Date(date),
+//       isCheat,
+//       userId,
+//     },
+//   });
+// }
+// // insertUser("sag", "spk21@gmail.com", "123456");
+// insertCheatMeal("2024-06-14", true, 1);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,39 @@
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import cors from "cors";
+const app = express();
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function insertUser(username: string, email: string, password: string) {
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password,
-    },
-  });
-  console.log(user);
-}
+app.use(cors());
+app.use(express.json());
 
-async function insertCheatMeal(date: string, isCheat: boolean, userId: number) {
-  const response = await prisma.cheatMeal.create({
-    data: {
-      date: new Date(date),
-      isCheat,
-      userId,
-    },
-  });
-}
+app.get("/register", (req: Request, res: Response) => {
+  res.json({ msg: "on register" });
+});
 
-// insertUser("sag", "spk21@gmail.com", "123456");
-insertCheatMeal("2024-06-13T16:30:00", true, 1);
+app.listen(3000, () => console.log("server up and running on port 3000"));
+
+// async function insertUser(username: string, email: string, password: string) {
+//   const user = await prisma.user.create({
+//     data: {
+//       username,
+//       email,
+//       password,
+//     },
+//   });
+//   console.log(user);
+// }
+
+// async function insertCheatMeal(date: string, isCheat: boolean, userId: number) {
+//   const response = await prisma.cheatMeal.create({
+//     data: {
+//       date: new Date(date),
+//       isCheat,
+//       userId,
+//     },
+//   });
+// }
+
+// // insertUser("sag", "spk21@gmail.com", "123456");
+// insertCheatMeal("2024-06-14", true, 1);
