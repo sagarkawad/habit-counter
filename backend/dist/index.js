de64539acc3b791fa279a9a84dbef513ad1bf478
sagzone <spkawad21@gmail.com>
2024-06-14 09:38:28 +0530

    expressified the register route

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,18 +1,49 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const cors_1 = __importDefault(require("cors"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const app = (0, express_1.default)();
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.get("/register", (req, res) => {
-    res.json({ msg: "on register" });
-});
+const JWT_SECRET = "secret";
+// Function to hash a password
+function hashPassword(plainPassword) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // Generate a salt
+            const salt = yield bcrypt_1.default.genSalt(10);
+            // Hash the password with the salt
+            const hashedPassword = yield bcrypt_1.default.hash(plainPassword, salt);
+            return hashedPassword;
+        }
+        catch (err) {
+            console.error("Error hashing password:", err);
+        }
+    });
+}
+app.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const email = req.body.email;
+    const password = yield hashPassword(req.body.password);
+    const token = jsonwebtoken_1.default.sign(email, JWT_SECRET);
+    console.log(email, password, token);
+    res.json({ token: token });
+}));
 app.listen(3000, () => console.log("server up and running on port 3000"));
 // async function insertUser(username: string, email: string, password: string) {
 //   const user = await prisma.user.create({
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import cors from "cors";
+import bcrypt from "bcrypt";
 const app = express();
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
@@ -8,8 +9,28 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
-app.get("/register", (req: Request, res: Response) => {
-  res.json({ msg: "on register" });
+const JWT_SECRET = "secret";
+
+// Function to hash a password
+async function hashPassword(plainPassword: string) {
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // Hash the password with the salt
+    const hashedPassword = await bcrypt.hash(plainPassword, salt);
+    return hashedPassword;
+  } catch (err) {
+    console.error("Error hashing password:", err);
+  }
+}
+
+app.post("/register", async (req: Request, res: Response) => {
+  const email = req.body.email;
+  const password = await hashPassword(req.body.password);
+  const token = jwt.sign(email, JWT_SECRET);
+  console.log(email, password, token);
+  res.json({ token: token });
+  
 });
 
 app.listen(3000, () => console.log("server up and running on port 3000"));
