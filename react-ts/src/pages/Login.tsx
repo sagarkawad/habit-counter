ea82ccb8eac7c8ad2282b36c8a836b56455cf17e
sagzone <spkawad21@gmail.com>
2024-06-15 19:38:29 +0530

    register and login page design created

--- react-ts/src/pages/Login.tsx
+++ react-ts/src/pages/Login.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+//import
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="grid w-full max-w-sm items-center gap-1.5 mb-4">
+        <Label htmlFor="email">UserName</Label>
+        <Input type="email" id="email" placeholder="Email / UserName" />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5 mb-4">
+        <Label htmlFor="pass">Password</Label>
+        <Input type="password" id="pass" placeholder="Password" />
+      </div>
+      <Button>Login</Button>
+      <p className="mt-14">
+        Not Registered?{" "}
+        <Link to="/register" className="text-gray-700">
+          Click here to Register
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
--- react-ts/src/pages/Register.tsx
+++ react-ts/src/pages/Register.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+//import
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="grid w-full max-w-sm items-center gap-1.5 mb-4">
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" id="email" placeholder="Email" />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5 mb-4">
+        <Label htmlFor="text">UserName</Label>
+        <Input type="text" id="text" placeholder="UserName" />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5 mb-4">
+        <Label htmlFor="pass">Password</Label>
+        <Input type="password" id="pass" placeholder="Password" />
+      </div>
+      <Button>Register</Button>
+      <p className="mt-14">
+        Already Registered?{" "}
+        <Link to="/login" className="text-gray-700">
+          Click here to Login
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
